3a12f37a016705f753eb5222f8406fd10116fd5d
xiah <xiah@rtruesoft.kr>
2020-03-17 20:42:16 +0900

    4# 화면전환-7

---
+++ Scene-CustomSegue/Scene-CustomSegue/NewSegue.swift
@@ -0,0 +1,26 @@
+//
+//  NewSegue.swift
+//  Scene-CustomSegue
+//
+//  Created by n on 17/03/2020.
+//  Copyright © 2020 n. All rights reserved.
+//
+
+import UIKit
+
+class NewSegue: UIStoryboardSegue {
+
+    
+    override func perform() {
+        
+        //세그웨이의 출발지 뷰 컨트롤러
+        let srcUVC = self.source
+        
+        //세그웨이의 목적지 뷰 컨트롤러
+        let destUVC = self.destination
+        
+        //fromView에서 toView로 뷰를 전환한다.
+        UIView.transition(from: srcUVC, to: destUVC, duration: 2, options: .transitionCurlDown, completion: nil)
+    }
+    
+}
---
+++ Scene-CustomSegue/Scene-CustomSegue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scene-CustomSegue
+//
+//  Created by n on 17/03/2020.
+//  Copyright © 2020 n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
