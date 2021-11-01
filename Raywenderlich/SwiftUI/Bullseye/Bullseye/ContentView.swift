37c21ca20c23a7ce670831c27fe7f7b78ee8ce8c
Xiah <xiah0526@gmail.com>
2021-11-01 11:55:54 +0900

    Raywenderlich - Challenge: SwiftUI State

--- Raywenderlich/SwiftUI/Bullseye/Bullseye/ContentView.swift
+++ Raywenderlich/SwiftUI/Bullseye/Bullseye/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     
     @State private var alertIsVisible: Bool = false
+    @State private var whosThereIsVisible: Bool = false
     
     var body: some View {
         VStack {
@@ -41,6 +42,14 @@ struct ContentView: View {
             }
             .alert(isPresented: $alertIsVisible, content: {
                 return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+            })
+            Button(action: {
+                self.whosThereIsVisible = true
+            }) {
+                Text("Knock Knock")
+            }
+            .alert(isPresented: $whosThereIsVisible, content: {
+                return Alert(title: Text("Who's there!"), message: Text("Little old lady"), dismissButton: .default(Text("Little old lady who?")))
             })
         }
     }
