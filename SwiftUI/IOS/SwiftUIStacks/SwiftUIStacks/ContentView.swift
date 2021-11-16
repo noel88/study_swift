3f9741c7066fc9bf5e2d96af73b8d205e22cfbba
Xiah <xiah0526@gmail.com>
2021-11-16 14:54:16 +0900

    Using spacer in VStack

--- SwiftUI/IOS/SwiftUIStacks/SwiftUIStacks/ContentView.swift
+++ SwiftUI/IOS/SwiftUIStacks/SwiftUIStacks/ContentView.swift
@@ -11,23 +11,26 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HeaderView()
-            HStack {
+            HStack(spacing: 15) {
                 PricingView(title:
                                 "Basic", price: "$9", textColor: .white, bgColor: .purple)
                 ZStack {
                     PricingView(title:
                                 "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
                     
-                    Text("Best for designer")
-                        .font(.system(.caption, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding(5)
-                        .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-                        .offset(x: 0, y: 87)
+                    ExtractedView(description: "Best for designer", offsetY: 87)
                 }
             }
             .padding(.horizontal)
+            
+            ZStack {
+                PricingView(title:
+                            "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                    .padding()
+                
+                ExtractedView(description: "Perfect for teams with 20 members", offsetY: 110)
+            }
+            Spacer()
         }
     }
 }
@@ -40,14 +43,19 @@ struct ContentView_Previews: PreviewProvider {
 
 struct HeaderView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 2) {
-            Text("Choose")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
-            Text("Your Plan")
-                .font(.system(.largeTitle, design: .rounded))
-                .fontWeight(.black)
+        
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Choose")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                Text("Your Plan")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+            }
+            Spacer()
         }
+        .padding()
     }
 }
 
@@ -57,9 +65,16 @@ struct PricingView: View {
     var price: String
     var textColor: Color
     var bgColor: Color
+    var icon: String?
     
     var body: some View {
         VStack {
+            icon.map({
+                Image(systemName: $0)
+                    .font(.largeTitle)
+                    .foregroundColor(textColor)
+            })
+            
             Text(title)
                 .font(.system(.title, design: .rounded))
                 .fontWeight(.black)
@@ -75,5 +90,21 @@ struct PricingView: View {
         .padding(40)
         .background(bgColor)
         .cornerRadius(10)
+    }
+}
+
+struct ExtractedView: View {
+    
+    var description: String
+    var offsetY: CGFloat
+    
+    var body: some View {
+        Text(description)
+            .font(.system(.caption, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .padding(5)
+            .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+            .offset(x: 0, y: offsetY)
     }
 }
