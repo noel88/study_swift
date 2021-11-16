13f8d1364bf0ed5d3afda00cec34e03cd7afac80
Xiah <xiah0526@gmail.com>
2021-11-16 14:11:33 +0900

    The Basic Plan

--- SwiftUI/IOS/SwiftUIStacks/SwiftUIStacks/ContentView.swift
+++ SwiftUI/IOS/SwiftUIStacks/SwiftUIStacks/ContentView.swift
@@ -9,6 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack {
+            HeaderView()
+            VStack {
+                Text("Basic")
+                    .font(.system(.title, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                Text("$9")
+                    .font(.system(size: 40, weight: .heavy, design: .rounded))
+                    .foregroundColor(.white)
+                Text("per month")
+                    .font(.headline)
+                    .foregroundColor(.white)
+            }
+            .padding(40)
+            .background(.purple)
+            .cornerRadius(10)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
         VStack(alignment: .leading, spacing: 2) {
             Text("Choose")
                 .font(.system(.largeTitle, design: .rounded))
@@ -17,11 +46,5 @@ struct ContentView: View {
                 .font(.system(.largeTitle, design: .rounded))
                 .fontWeight(.black)
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
