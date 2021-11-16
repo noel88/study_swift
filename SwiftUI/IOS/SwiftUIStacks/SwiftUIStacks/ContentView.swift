3a40b333e337f96befd7ea099800ede6a3c5c0ca
Xiah <xiah0526@gmail.com>
2021-11-16 14:25:16 +0900

    Position the text view using .offset

--- SwiftUI/IOS/SwiftUIStacks/SwiftUIStacks/ContentView.swift
+++ SwiftUI/IOS/SwiftUIStacks/SwiftUIStacks/ContentView.swift
@@ -14,8 +14,18 @@ struct ContentView: View {
             HStack {
                 PricingView(title:
                                 "Basic", price: "$9", textColor: .white, bgColor: .purple)
-                PricingView(title:
+                ZStack {
+                    PricingView(title:
                                 "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                    
+                    Text("Best for designer")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                        .offset(x: 0, y: 87)
+                }
             }
             .padding(.horizontal)
         }
