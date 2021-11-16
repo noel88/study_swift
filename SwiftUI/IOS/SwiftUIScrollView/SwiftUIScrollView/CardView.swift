6e676820061e98d9a2f873677daf02f875bdb622
Xiah <xiah0526@gmail.com>
2021-11-16 15:17:32 +0900

    Adjusting the layout priority

--- SwiftUI/IOS/SwiftUIScrollView/SwiftUIScrollView/CardView.swift
+++ SwiftUI/IOS/SwiftUIScrollView/SwiftUIScrollView/CardView.swift
@@ -13,20 +13,32 @@ struct CardView: View {
             Image("swiftui-button")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            VStack(alignment: .leading) {
-                Text("SwiftUI")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                Text("Drawing a Border with Rounded Corners")
-                    .font(.title)
-                    .fontWeight(.black)
-                    .foregroundColor(.primary)
-                    .lineLimit(3)
-                Text("Written by Simon Ng".uppercased())
-                    .font(.caption)
-                    .foregroundColor(.secondary)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("SwiftUI")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text("Drawing a Border with Rounded Corners")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("Written by Simon Ng".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                .layoutPriority(100)
+                Spacer()
             }
+            .padding()
         }
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
+        
     }
 }
 
